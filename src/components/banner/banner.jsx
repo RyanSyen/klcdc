89a0f9506d523daaf044edacdf85d4bbbe876e1d
Ryan Wong <ryan.wong@microlink.com.my>
2024-08-25 16:46:27 +0800

    fix background color overwrite image

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -18,7 +18,7 @@ const Banner = () => {
           fill
           priority
         />
-        <div className="absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.3)] bg-slate-500 bg-blend-multiply" />
+        <div className="absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.3)] bg-blend-multiply" />
         <div className="absolute top-[25%] inset-0 flex items-center justify-center z-[2] text-white text-center px-4">
           <div className="max-w-xxl">
             <h1 className="text-3xl lg:text-5xl font-bold">
