26b40ee0e84dc1af46aa8ba98b5b0a2b1fe9c629
Ryan Wong <ryanwong710200@gmail.com>
2024-10-07 22:36:41 +0800

    added location details for geolocation seo

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -11,6 +11,24 @@ import { MessageCircleMore, Phone, Mail, Church, Instagram } from "lucide-react"
 import Link from "next/link";
 import { LinkPreview } from "@/components/ui/link-preview";
 
+export const metadata = {
+  title: "Kuala Lumpur Christian Disciples Church",
+  description:
+    "Discover Kuala Lumpur Christian Disciples Church (KLCDC), a welcoming community located in Kuchai Lama. Join us for our services and connect with fellow believers. Easily accessible from Sri Petaling and surrounding areas, we invite you to visit us for spiritual growth and fellowship. Find directions and contact details on our website.",
+  keywords: [
+    "kuala lumpur",
+    "church",
+    "kuchai lama",
+    "community",
+    "friendly church",
+    "loving community",
+    "Christian church in Kuala Lumpur",
+    "church near Sri Petaling",
+    "Kuchai Lama church",
+    "KLCDC"
+  ],
+};
+
 const Contact = () => {
   return (
     <section className="py-20">
@@ -21,8 +39,11 @@ const Contact = () => {
         <div className="pt-8">
           <h2 className="scroll-m-20 pb-4 text-3xl font-semibold tracking-tight first:mt-0">Contact Us</h2>
         </div>
-        <div className="grid grid-cols-[repeat(auto-fit, minmax(16rem, 24rem))] gap-1">
-          <Card>
+        <div className="pb-4">
+           Kuala Lumpur Christian Disciples Church (KLCDC) is located in the heart of Kuchai Lama, Kuala Lumpur. We are easy to find and accesible from major roads and public transport. Whether you are visiting from nearby areas like Sri Petaling, Old Klang Road, we invite you to join us for our service. For directions, parking details or any other inquiries, feel free to reach out to us via the contact details below. We look forward to seeing you Christian Disciples Church Kuala Lumpur! 
+        </div>
+        <div className="flex flex-wrap items-center gap-1">
+          <Card className="w-[300px] h-[270px]">
             <CardHeader>
               <MessageCircleMore width={64} height={64} />
               <CardTitle className="pt-4">Pastor Peter Wong</CardTitle>
@@ -34,21 +55,21 @@ const Contact = () => {
               <Mail /> &nbsp; petroswcm@gmail.com
             </CardFooter>
           </Card>
-          <Card>
+          <Card className="w-[300px] h-[270px]">
             <CardHeader>
               <Church width={64} height={64} />
-              <CardTitle className="pt-4">Address</CardTitle>
+              <CardTitle className="pt-4 flex gap-4">
+                Address               
+                <Link href={"https://maps.app.goo.gl/3o8DJWt6tGq2xdEE9"} target="_blank"><Image src="/sports/google-maps.png" height={24} width={24} alt="Google Maps" /></Link>
+              </CardTitle>
             </CardHeader>
             <CardContent>
               <p>30-2, Jalan Kuchai Maju 11,
                 Kuchai Entrepreneurs Park,
                 58200 Kuala Lumpur</p>
             </CardContent>
-            <CardFooter>
-              <Link href={"https://maps.app.goo.gl/3o8DJWt6tGq2xdEE9"} target="_blank"><Image src="/sports/google-maps.png" height={24} width={24} alt="Google Maps" /></Link>
-            </CardFooter>
           </Card>
-          <Card>
+          <Card className="w-[300px] h-[270px]">
             <CardHeader>
               <Instagram size={48} strokeWidth={2.25} />
               <CardTitle className="pt-4">Instagram</CardTitle>
